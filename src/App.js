9ad5dfefdb74bbaa50ec98a94bcc2b59633a7ac8
Marosh Sykora <sykoramaros@gmail.com>
2024-12-21 14:20:38 +0100

    Vytvorena zakladni kostra Pages a komponenty Layout Navbar a Footer

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.css"
+import Layout from "../src/Components/Layout/Layout"
+import Home from "../src/Pages/Home/Home"
+import O_me from "../src/Pages/O_me/O_me"
+import Znalosti from "../src/Pages/Znalosti/Znalosti"
+import Projekty from "../src/Pages/Projekty/Projekty"
+import Konicky from "../src/Pages/Konicky/Konicky"
+import Kontakt from "../src/Pages/Kontakt/Kontakt"
 
 const App = () => {
   return (
-    <div>
-      <p className="fs-1 text-primary">Click</p>
-      <p className="fs-1 text-secondary">Click</p>
-      <p className="fs-1 text-success">Click</p>
-      <p className="fs-1 text-danger">Click</p>
-      <p className="fs-1 text-warning">Click</p>
-      <p className="fs-1 text-info">Click</p>
-      <p className="fs-1 text-light">Click</p>
-      <p className="fs-1 text-dark">Click</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/o_me" element={<O_me />} />
+          <Route path="/znalosti" element={<Znalosti />} />
+          <Route path="/projekty" element={<Projekty />} />
+          <Route path="/konicky" element={<Konicky />} />
+          <Route path="/kontakt" element={<Kontakt />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
