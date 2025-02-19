ac3eeb6306dbafdc4f5e00aee9205ec6918729d6
Marosh Sykora <sykoramaros@gmail.com>
2025-02-19 23:51:22 +0100

    Do Footer.jsx pridan W3C css validator

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,8 +3,23 @@ import React from "react"
 const Footer = () => {
   return (
     <div>
-      <noscript>Prazdny transparentni div footer (zatim)</noscript>
-      <div className="bg-transparent" style={{ height: "100px" }}></div>
+      <div
+        className="bg-transparent d-flex justify-content-start align-items-end"
+        style={{ height: "100px" }}
+      >
+        <div className="ms-5">
+          <p>
+            <a href="https://jigsaw.w3.org/css-validator/check/referer">
+              <img
+                className="opacity-75"
+                style={{ border: "0", width: "88px", height: "31px" }}
+                src="https://jigsaw.w3.org/css-validator/images/vcss-blue"
+                alt="Valid CSS!"
+              />
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
