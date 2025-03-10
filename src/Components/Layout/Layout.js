fb2c488ec4fee174736a4486f20e7a7a14061968
Marosh Sykora <sykoramaros@gmail.com>
2025-03-10 16:55:23 +0100

    pridano do Layout svg pozadi

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -6,7 +6,8 @@ const Layout = () => {
   return (
     <div>
       <Navbar />
-      <div style={{ margin: "150px 0" }}>
+      <div className="position-relative" style={{ margin: "150px 0" }}>
+          <img className="position-absolute w-100 d-none d-lg-block" style={{opacity: "0.07", top: "-100px", zIndex: "-999"}} src={`${process.env.PUBLIC_URL}/img/background.svg`} alt="Background"/>
         <Outlet />
       </div>
       <Footer />
